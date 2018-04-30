f92c66814defdf2763757d84435fd6f0c08fb808
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-04-30 23:09:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,5 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   var index = 0;
-  document.addEventListenter('keydown',function(e))
+  document.addEventListenter('keydown',function(e) {
 }
