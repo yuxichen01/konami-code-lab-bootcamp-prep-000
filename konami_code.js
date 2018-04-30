953988e07cd3050a70322090441741ea2e31c8b1
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-04-30 23:15:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   var index = 0;
-  document.addEventListenter('keydown',function(e) {
+  document.body.addEventListenter('keydown',function(e) {
     const key = parseInt(e.detail || e.which);
-    if (key === code[index])
+    if (key === code[index]) {
+      index++;
+      if (index ===code.length) {
+        alert('CONGRATS!');
+        index = 0;
+      }
+  } else {
+    index = 0;
   }
 }
